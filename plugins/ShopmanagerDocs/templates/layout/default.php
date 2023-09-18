88277242a67a77f7277ba73aa10f36be0cf1aa5f
Tim Bischoff - Software Developer <tim.bischoff@bisweb.me>
2023-09-18 17:35:57 +0200

    Optimize Navbar on Mobile

--- plugins/ShopmanagerDocs/templates/layout/default.php
+++ plugins/ShopmanagerDocs/templates/layout/default.php
@@ -39,17 +39,23 @@ $this->Html->setTemplates([
     <nav class="site top-nav">
         <div class="top-nav-title">
             <a href="<?= $this->Url->build('/') ?>" class="" title="&Uuml;bersicht Dokumentation Shopmanager Plugin">
-                <span class="leading-normal font-heading">Dokumentation Shopmanager Plugin</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[20px] h-[20px] inline sm:hidden">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                </svg>
+                <span class="leading-normal font-heading hidden sm:inline">Dokumentation</span>
+                <span class="leading-normal font-heading">Shopmanager Plugin</span>
             </a>
         </div>
         <div class="top-nav-links">
             <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/" title="CakePHP Framework Kochbuch">
-                <span class="cakered">Cake</span><span class="cakegray">PHP</span>
-                <span class="font-normal">Dokumentation</span>
+                <?= $this->Html->image('/shopmanager_docs/img/cake.png', ['alt' => 'CakePHP Icon', 'title' => '', 'class' => 'p-[2px] cake-bg-red inline mr-1 sm:hidden', 'height' => '20', 'width' => '20']) ?>
+                <span class="cakered hidden sm:inline">Cake</span><span class="cakegray hidden sm:inline">PHP</span>
+                <span class="cakegray sm:black font-bold sm:font-normal">Kochbuch</span>
             </a>
             <a target="_blank" rel="noopener" href="https://api.cakephp.org/4.4/" title="CakePHP Framework API Referenz">
-                <span class="cakered">Cake</span><span class="cakegray">PHP</span>
-                <span class="font-normal">API</span>
+                <?= $this->Html->image('/shopmanager_docs/img/cake.png', ['alt' => 'CakePHP Icon', 'title' => '', 'class' => 'p-[2px] cake-bg-red inline mr-1 sm:hidden', 'height' => '20', 'width' => '20']) ?>
+                <span class="cakered hidden sm:inline">Cake</span><span class="cakegray hidden sm:inline">PHP</span>
+                <span class="cakegray sm:black font-bold sm:font-normal">API</span>
             </a>
         </div>
     </nav>
