39a6dee55006ec9358cefad5e89b41d14f95f234
Tim Bischoff - Software Developer <tim.bischoff@bisweb.me>
2023-12-31 15:52:09 +0100

    Update Pirsch.io as own Dashboard

--- plugins/ShopmanagerDocs/templates/element/pirsch.php
+++ plugins/ShopmanagerDocs/templates/element/pirsch.php
@@ -1,1 +1,1 @@
-<script defer type="text/javascript" src="https://api.pirsch.io/pirsch-extended.js" id="pirschextendedjs" data-code="5rr3vOTRLjVcCWbVrP0leGlvBiaGhnsx"></script>
+<script defer type="text/javascript" src="https://api.pirsch.io/pirsch-extended.js" id="pirschextendedjs" data-code="OA7brDRX2NzGULXcdUovzbn0xUju34fI"></script>
